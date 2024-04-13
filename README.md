9dfb66dfcfbb038c4f79215a285d61ca58a903a6
KidJoe1412 <KidJoe1412@163.com>
2024-04-13 19:41:15 +0800

    feature：项目初始化

---
+++ README.md
@@ -0,0 +1,2 @@
+后端地址：localhost:8081/shop-type/list
+前端地址：localhost:8080
---
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -0,0 +1,20 @@
+package com.hmdp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.hmdp.mapper")
+@SpringBootApplication
+@Slf4j
+public class HmDianPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmDianPingApplication.class, args);
+        log.info("0000000000000000000000000000000000000000000000000000000000000000000000000000000");
+        log.info("0000000000000000000000000000000000000000000000000000000000000000000000000000000");
+        log.info("0000000000000000000000000000000000000000000000000000000000000000000000000000000");
+    }
+
+}
