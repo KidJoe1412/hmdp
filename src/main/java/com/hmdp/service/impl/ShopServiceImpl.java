22459c027bcaf42b014b757c16c41e72d3634103
KidJoe1412 <KidJoe1412@163.com>
2024-04-20 16:12:01 +0800

    feature：商铺缓存，解决缓存穿透（往redis添加空对象解决）

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -13,10 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -43,15 +43,33 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
-
+        // 判断命中的是否是空字符串(将空值写入redis，避免缓存穿透时，存入的)
+        if ("".equals(shopJson)){
+            return Result.fail("店铺不存在");
+        }
         // 3.未查询到,根据id mysql查询
         Shop shop = getById(id);
         // 4.不存在，返回错误
         if (shop == null){
+            // 将空值写入redis，避免缓存穿透
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
             return Result.fail("店铺不存在");
         }
         // 5.存在，写入redis，返回
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL,TimeUnit.MINUTES);
         return Result.ok(shop);
+    }
+
+    @Override
+    public Result update(Shop shop) {
+        // 1.先操作数据库
+        Long id = shop.getId();
+        if (id == null){
+            return Result.fail("店铺id不能为空");
+        }
+        updateById(shop);
+        // 2.再删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
+        return Result.ok();
     }
 }
